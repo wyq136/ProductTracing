d6d192ca0978c7599cf2e6472912de829fc8bc1d
hf136 <hf.13699@gmail.com>
2015-08-20 16:20:12 +0800

    merge mybatis and add local jar

--- src/main/java/cn/edu/bit/linc/controller/HelloWorld.java
+++ src/main/java/cn/edu/bit/linc/controller/HelloWorld.java
@@ -1,7 +1,14 @@
 package cn.edu.bit.linc.controller;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.edu.bit.linc.pojo.User;
+import cn.edu.bit.linc.util.DBUtil;
 
 @Controller
 public class HelloWorld {
@@ -10,5 +17,16 @@ public class HelloWorld {
 	public String sayHello(){
 		System.out.println("hello!");
 		return "success";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/testMybatis")
+	public List<User> testMybatis(){
+		SqlSession session = DBUtil.openSession();
+		User user = session.selectOne("cn.edu.bit.linc.pojo.User.selectUserByUsername", "user01");
+		List<User> users = session.selectList("cn.edu.bit.linc.pojo.User.selectUser");
+		System.out.println(users.size());
+		System.out.println(user);
+		return users;
 	}
 }
