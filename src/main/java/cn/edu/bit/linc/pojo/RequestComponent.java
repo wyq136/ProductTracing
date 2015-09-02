cba19a4ff501e419dfb662093f05a335acbf939a
hf136 <hf.13699@gmail.com>
2015-09-02 17:42:54 +0800

    Fix: 编码问题。 Add： 不喜欢成分。

---
+++ src/main/java/cn/edu/bit/linc/pojo/RequestComponent.java
@@ -0,0 +1,22 @@
+package cn.edu.bit.linc.pojo;
+
+public class RequestComponent {
+
+	private Component[] like;
+	private Component[] dislike;
+	
+	public Component[] getLike() {
+		return like;
+	}
+	public void setLike(Component[] like) {
+		this.like = like;
+	}
+	public Component[] getDislike() {
+		return dislike;
+	}
+	public void setDislike(Component[] dislike) {
+		this.dislike = dislike;
+	}
+	
+	
+}
