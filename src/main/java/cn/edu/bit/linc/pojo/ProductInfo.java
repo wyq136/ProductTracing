5066c76d944311788b8a4169fc6a915191332499
hf136 <hf.13699@gmail.com>
2015-09-06 18:38:18 +0800

    Fix: 图片上传路径错误。 Add: 随机返回20条component

--- src/main/java/cn/edu/bit/linc/pojo/ProductInfo.java
+++ src/main/java/cn/edu/bit/linc/pojo/ProductInfo.java
@@ -5,6 +5,8 @@ public class ProductInfo {
 	private int product_id;
 	private String component_id;
 	private String catalog_id;
+	private String component_name;
+	private String catalog_name;
 	
 	public int getProduct_id() {
 		return product_id;
@@ -23,6 +25,18 @@ public class ProductInfo {
 	}
 	public void setCatalog_id(String catalog_id) {
 		this.catalog_id = catalog_id;
+	}
+	public String getComponent_name() {
+		return component_name;
+	}
+	public void setComponent_name(String component_name) {
+		this.component_name = component_name;
+	}
+	public String getCatalog_name() {
+		return catalog_name;
+	}
+	public void setCatalog_name(String catalog_name) {
+		this.catalog_name = catalog_name;
 	}
 	
 }
